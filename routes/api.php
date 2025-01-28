3e2e30bd551438501fcaddf84890ba4ed8c33381
mwiko <mwikosinyangwe@gmail.com>
2025-01-28 10:05:36 +0100

    what

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/user', function () {
+    return 'API';
+})->middleware('auth:sanctum');
