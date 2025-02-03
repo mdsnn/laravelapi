84c968e35707ef560df1996c4bbbff22c243dc81
mwiko sinyangwe <mwikosinyangwe@gmail.com>
2025-02-03 08:17:18 +0200

    ..

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/posts', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
