ab217cfa4fd81a33861231098d21c89085b49d19
mwiko sinyangwe <mwikosinyangwe@gmail.com>
2025-02-20 09:34:13 +0200

    ..

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/posts', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
